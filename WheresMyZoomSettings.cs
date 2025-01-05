8f2ccb238d0c4e19c4dcbc566acaa417c9253ecd
Larsonix <148280456+Larsonix@users.noreply.github.com>
2025-01-05 10:54:50 +0100

    Added a check button for each setting to automatically activate at launch
    Added

--- WheresMyZoomSettings.cs
+++ WheresMyZoomSettings.cs
@@ -8,8 +8,13 @@ public class WheresMyZoomAtSettings : ISettings
     public ToggleNode Enable { get; set; } = new ToggleNode(false);
 
     public ButtonNode EnableZoom { get; set; } = new ButtonNode();
+    public ToggleNode EnableZoomAtLaunch { get; set; } = new ToggleNode(false);
     public ButtonNode EnableFastZoom { get; set; } = new ButtonNode();
+    public ToggleNode EnableFastZoomAtLaunch { get; set; } = new ToggleNode(false);
     public ButtonNode EnableNoFog { get; set; } = new ButtonNode();
+    public ToggleNode EnableNoFogAtLaunch { get; set; } = new ToggleNode(false);
     public ButtonNode EnableNoBlackBox { get; set; } = new ButtonNode();
+    public ToggleNode EnableNoBlackBoxAtLaunch { get; set; } = new ToggleNode(false);
     public ButtonNode EnableBrightness { get; set; } = new ButtonNode();
+    public ToggleNode EnableBrightnessAtLaunch { get; set; } = new ToggleNode(false);
 }
