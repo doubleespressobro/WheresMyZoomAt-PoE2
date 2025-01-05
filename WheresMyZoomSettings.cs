ad4f8c5dae29fe87948f79fc2f1be3bc7c783091
Larsonix <148280456+Larsonix@users.noreply.github.com>
2025-01-05 11:05:48 +0100

    Typo for readability

--- WheresMyZoomSettings.cs
+++ WheresMyZoomSettings.cs
@@ -9,12 +9,16 @@ public class WheresMyZoomAtSettings : ISettings
 
     public ButtonNode EnableZoom { get; set; } = new ButtonNode();
     public ToggleNode EnableZoomAtLaunch { get; set; } = new ToggleNode(false);
+
     public ButtonNode EnableFastZoom { get; set; } = new ButtonNode();
     public ToggleNode EnableFastZoomAtLaunch { get; set; } = new ToggleNode(false);
+
     public ButtonNode EnableNoFog { get; set; } = new ButtonNode();
     public ToggleNode EnableNoFogAtLaunch { get; set; } = new ToggleNode(false);
+
     public ButtonNode EnableNoBlackBox { get; set; } = new ButtonNode();
     public ToggleNode EnableNoBlackBoxAtLaunch { get; set; } = new ToggleNode(false);
+
     public ButtonNode EnableBrightness { get; set; } = new ButtonNode();
     public ToggleNode EnableBrightnessAtLaunch { get; set; } = new ToggleNode(false);
 }
