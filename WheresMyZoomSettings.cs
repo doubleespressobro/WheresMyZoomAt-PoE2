476c8e75428a37fd3858bca3e394f2cb422fbce0
Larsonix <xxredlaboxx@gmail.com>
2025-01-06 22:21:52 +0100

    Added Readability + User settings for Atlas Zoom + Redid every reference + Created Unzoom feature (espressolove) + Found a way to continuously write to the atlas zoom value but it's probably fucked up. Number 2

--- WheresMyZoomSettings.cs
+++ WheresMyZoomSettings.cs
@@ -1,6 +1,7 @@
 ﻿using ExileCore2.Shared.Interfaces;
 using ExileCore2.Shared.Nodes;
 using ExileCore2.Shared.Attributes;
+using System.Runtime;
 
 namespace WheresMyZoomAt;
 
@@ -8,17 +9,39 @@ public class WheresMyZoomAtSettings : ISettings
 {
     public ToggleNode Enable { get; set; } = new ToggleNode(false);
 
+    [Menu("Zoom Menu")]
+    public ZoomMenu ZoomMenu { get; set; } = new ZoomMenu();
+
+    [Menu("QOL Menu")]
+    public QOLMenu QOLMenu { get; set; } = new QOLMenu();
+}
+
+[Submenu(CollapsedByDefault = false)]
+public class ZoomMenu
+{
+    [Menu("Enable Unzoom", "Scroll Mouse Wheel to unzoom further.")]
     public ButtonNode EnableZoom { get; set; } = new ButtonNode();
+    [Menu("Enable Unzoom at Launch", "Scroll Mouse Wheel to unzoom further.")]
     public ToggleNode EnableZoomAtLaunch { get; set; } = new ToggleNode(false);
 
+    [Menu("Enable Fast Zoom", "Scroll Mouse Wheel unzoom faster.")]
     public ButtonNode EnableFastZoom { get; set; } = new ButtonNode();
+    [Menu("Enable Fast Zoom at Launch", "Scroll Mouse Wheel unzoom faster.")]
     public ToggleNode EnableFastZoomAtLaunch { get; set; } = new ToggleNode(false);
 
-    [Menu("Enable Atlas Unzoom", "DOESN'T WORK !")]
+    [Menu("Enable Atlas Unzoom", "Press the button and then drag the Atlas for it to unzoom.")]
     public ButtonNode EnableAtlasZoom { get; set; } = new ButtonNode();
-    [Menu("Enable Atlas Unzoom at launch", "DOESN'T WORK !")]
+    [Menu("Enable Atlas Unzoom at launch", "Press the button and then drag the Atlas for it to unzoom.")]
     public ToggleNode EnableAtlasZoomAtLaunch { get; set; } = new ToggleNode(false);
-    
+    [Menu("Keep Unzoom Always running", "Press the button and then drag the Atlas for it to unzoom.")]
+    public ToggleNode KeepAtlasZoom { get; set; } = new ToggleNode(false);
+    [Menu("Atlas Unzoom Value", "The Higher the value, the more zoomed in.")]
+    public RangeNode<float> AtlasUnzoomValue { get; set; } = new RangeNode<float>(1.0f, 5f, 0.5f);
+}
+
+[Submenu(CollapsedByDefault = false)]
+public class QOLMenu
+{
     public ButtonNode EnableNoFog { get; set; } = new ButtonNode();
     public ToggleNode EnableNoFogAtLaunch { get; set; } = new ToggleNode(false);
 
